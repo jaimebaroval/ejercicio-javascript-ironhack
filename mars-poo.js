e7647f5a9403c586101432703896bcf44be0f413
Jaime Baro <jaimebaroval@gmail.com>
2020-04-03 23:05:59 +0200

    version 2.1 Limite tabla Abajo OK

--- mars-poo.js
+++ mars-poo.js
@@ -13,12 +13,14 @@ const grid = [
     [90, 91, 92, 93, 94, 95, 96, 97, 98, 99],
 ]
 
-const obstaculos = [74, 11, 24, 92, 87, 84, 61, 17, 43]
+const obstaculos = [74, 11, 24, 92, 89, 84, 61, 17, 43]
 
-const commandsArray = ['f', 'f', 'f', 'f', 'f', 'f', 'l', 'f', 'f', 'f', 'l', 'f', 'r', 'f', 'f', 'l', 'f', 'f', 'f', 'r', 'f', 'f', 'f', 'r', 'f', 'l', 'f', 'f', 'f'];
+const commandsArray = ['f', 'f', 'f', 'f', 'f', 'f', 'f', 'f', 'f', 'f', 'r', 'f', 'f', 'l', 'f', 'f', 'f', 'r', 'f', 'f', 'f', 'r', 'f', 'l', 'f', 'f', 'f'];
 // let commandsArray = ['b', 'b', 'b', 'b', 'b', 'b', 'l', 'f'];
 
-const commandsArray2 = ['f', 'l', 'f', 'r', 'f', 'f', 'f', 'f', 'l', 'f', 'f', 'f', 'l', 'f', 'f', 'f', 'f', 'r', 'f', 'f', 'l', 'f'];
+const commandsArray2 = ['f', 'f', 'f', 'l', 'f', 'f', 'f', 'f', 'l', 'f', 'f', 'f', 'l', 'f', 'f', 'f', 'f', 'r', 'f', 'f', 'f', 'f', 'f', 'r', 'f', 'f', 'l', 'f', 'f'];
+
+const commandsTurns = []
 
 
 
@@ -86,7 +88,6 @@ tableCreate();
 
 
 
-
 // Object roverObject()
 
 function roverObject(commands, startDirection, posX, posY) {
@@ -100,6 +101,8 @@ function roverObject(commands, startDirection, posX, posY) {
         x: posX,
         y: posY
     };
+
+    let fueraTabla = false;
 
     const positionLog = []
 
@@ -123,6 +126,14 @@ function roverObject(commands, startDirection, posX, posY) {
 
         let analisisCelda = analizarTerreno(command, roverDirection)
 
+        // Girar
+        if (command == 'l' || command == 'r') {
+
+            girarRover(command, roverDirection)
+
+
+        }
+
         if (analisisCelda) {
 
             console.log('Posición actual: ' + grid[newPos.y][newPos.x]);
@@ -134,14 +145,11 @@ function roverObject(commands, startDirection, posX, posY) {
                 retroceder(roverDirection)
             }
 
-            // Girar
-            if (command == 'l' || command == 'r') {
-                girarRover(command)
-            }
-
-            pintarRover();
-
         }
+
+
+
+        pintarRover();
     }
 
     function avanzar(roverDirection) {
@@ -209,35 +217,49 @@ function roverObject(commands, startDirection, posX, posY) {
 
             console.log('Giro Izquierda');
             console.log('·····························');
-
         }
 
     }
 
     function analizarTerreno(command, roverDirection) {
 
+        let posicionActual = grid[newPos.y][newPos.x]
         let nuevaPosicion
         let celdaSiguiente
 
-        if (command == 'f') {
+
+
+
+        if (command == 'f' && grid[newPos.y][newPos.x] != null) {
+
+            console.log(grid[newPos.y][newPos.x] < 90);
+            console.log(posicionActual);
+
 
             nuevaPosicion =
                 roverDirection == 'N' ? grid[newPos.y - 1][newPos.x] :
-                    roverDirection == 'S' ? grid[newPos.y + 1][newPos.x] :
-                        roverDirection == 'E' ? grid[newPos.y][newPos.x + 1] :
+                    roverDirection == 'S' && grid[newPos.y][newPos.x] < 90 ? grid[newPos.y + 1][newPos.x] :
+                        roverDirection == 'E' && grid[newPos.y][newPos.x] < 90 ? grid[newPos.y][newPos.x + 1] :
                             roverDirection == 'W' ? grid[newPos.y][newPos.x - 1] :
                                 null;
+
 
         } else if (command == 'b') {
 
             nuevaPosicion =
-                roverDirection == 'N' ? grid[newPos.y + 1][newPos.x] :
+                roverDirection == 'N' && grid[newPos.y][newPos.x] < 90 ? grid[newPos.y + 1][newPos.x] :
                     roverDirection == 'S' ? grid[newPos.y - 1][newPos.x] :
                         roverDirection == 'E' ? grid[newPos.y][newPos.x - 1] :
-                            roverDirection == 'W' ? grid[newPos.y][newPos.x + 1] :
+                            roverDirection == 'W' && grid[newPos.y][newPos.x] < 90 ? grid[newPos.y][newPos.x + 1] :
                                 null;
 
         }
+
+
+
+
+
+
 
         celdaSiguiente = document.getElementById(nuevaPosicion)
 
@@ -257,6 +279,8 @@ function roverObject(commands, startDirection, posX, posY) {
             console.log('-----------------------------');
             return 0
         }
+
+
 
 
     }
@@ -286,12 +310,46 @@ function roverObject(commands, startDirection, posX, posY) {
 console.log(`ROVER 1 =>
 
 `);
-var Rover1 = new roverObject(commandsArray, 'N', 9, 6)
+var Rover1 = new roverObject(commandsArray, 'E', 6, 5)
 
 console.log(`
 
 ROVER 2 =>
 
 `);
-var Rover2 = new roverObject(commandsArray2, 'N', 9, 9) // Rover 2 Cae sobre Rover 1
+var Rover2 = new roverObject(commandsArray2, 'W', 6, 4) // Rover 2 Cae sobre Rover 1
 
+
+
+function avancePorTurnos() {
+
+    console.log('Array 1: ');
+
+    // commandsArray.forEach(k => {
+    //     commandsArray2.forEach(i => {
+    //         console.log(i, k)
+    //     });
+    // };
+
+    commandsArray.forEach((num1, index) => {
+        const num2 = commandsArray2[index];
+        commandsTurns.push(num1)
+        commandsTurns.push(num2)
+    });
+
+    // commandsArray.forEach(function (e, idx) {
+    //         console.log('[' + idx + ']' + commandsArray[idx] + ' ')
+
+    // })
+
+
+
+    // commandsArray2.forEach(function (i) {
+    //     commandsTurns.push(i)
+    // })
+
+    console.log(commandsTurns);
+
+}
+
+avancePorTurnos()
